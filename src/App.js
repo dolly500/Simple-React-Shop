726c9d931bf8801796f21744c00202398a260a63
Akamo Dolapo Samson <61446935+dolly500@users.noreply.github.com>
2021-10-23 17:50:39 +0100

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import './App.css';
+
+import HomePage from './components/pages/homepage/homepage.component';
+
+function App() {
+    return ( <
+        div className = "App" >
+        <
+        div >
+        <
+        HomePage / >
+        <
+        /div> <
+        /div>
+    );
+}
+
+export default App;
